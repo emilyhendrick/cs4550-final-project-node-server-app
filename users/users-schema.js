93c1788b62eeb5759516ae35aff6fe210da8d923
emilyhendrick <emhendrick31@gmail.com>
2023-04-22 18:26:13 -0400

    add business users to user schema

--- users/users-schema.js
+++ users/users-schema.js
@@ -5,10 +5,13 @@ const usersSchema = new mongoose.Schema(
       username: {type: String, required: true, unique: true},
       password: {type: String, required: true},
       firstName: {type: String, default: ""},
-      lastName: String,
+      lastName: {type: String, default: ""},
       email: {type: String},
       createdOn: {type: Date, default: Date.now},
       profilePic: {type: String},
+      userRole: {type: String, required: true},
+      businessName: {type: String},
+      businessAddress: {type: String}
     },
     {collection: "users"}
 );
