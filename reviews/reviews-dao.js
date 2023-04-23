bf344f0a40afe393bcc7f834720d5067727ce135
emilyhendrick <emhendrick31@gmail.com>
2023-04-22 23:08:43 -0400

    update reviews controller

--- reviews/reviews-dao.js
+++ reviews/reviews-dao.js
@@ -7,3 +7,27 @@ export const createReview = async (review) => {
   const newReview = await reviewModel.create(review);
   return newReview;
 };
+export const updateReview = async (id, review) => {
+  const status = await reviewModel.updateOne({ _id: id }, review);
+  return status;
+};
+
+export const deleteReview = async (id) => {
+  const status = await reviewModel.deleteOne({ _id: id });
+  return status;
+};
+
+export const findAllReviews = async () => {
+  const reviews = await reviewModel.find();
+  return reviews;
+};
+
+export const findReviewByUsername = async (username) => {
+  const review = await reviewModel.find({ reviewer: username });
+  return review;
+};
+
+export const findReviewByRestaurant = async (username) => {
+  const review = await reviewModel.find({ restaurantName: username });
+  return review;
+};
--- reviews/reviews-schema.js
+++ reviews/reviews-schema.js
@@ -1,15 +1,16 @@
 import mongoose from "mongoose";
 
 const reviewSchema = new mongoose.Schema(
-  {
-    reviewer: {type: String, required: true},
-    reviewId: {type: String , required: true, unique: true},
-    review: {type: String, required: true},
-    Rating: {type: Number},
-    createdOn: {type: Date, default: Date.now},
-    profilePic: {type: String},
-  },
-  { collection: "reviews" }
+    {
+      reviewer: {type: String, required: true},
+      reviewId: {type: String, required: true, unique: true},
+      review: {type: String, required: true},
+      Rating: {type: Number},
+      createdOn: {type: Date, default: Date.now},
+      profilePic: {type: String},
+      restaurantName: {type: String, required: true}
+    },
+    {collection: "reviews"}
 );
 
 export default reviewSchema;
